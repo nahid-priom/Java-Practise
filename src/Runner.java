084cef978cb4817e4f0953f2c538a9b754897f0a
nahid-priom <nahid.priom.06@gmail.com>
2023-08-23 17:18:50 +0600

    Linked List insertion

---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(20);
+        list.insert(20);
+        list.insert(20);
+        
+       
+
+        list.Show();
+    }
+}
