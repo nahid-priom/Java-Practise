b4c10d484e49c42a25cc1590bec6ff8dafca5718
nahid-priom <nahid.priom.06@gmail.com>
2023-08-26 22:18:01 +0600

    finding index by binary search

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -12,59 +12,20 @@ public class LinkedList {
             Node n = head;
             while (n.next != null) {
                 n = n.next;
+
             }
             n.next = node;
         }
-    }
 
-    public void insertAtFirst(int data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-
-        node.next = head;
-        head = node;
-    }
-
-    public void insertAtInex(int index, int data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-
-        if (index == 0) {
-            insertAtFirst(data);
-        } else {
-            Node n = head;
-            for (int i = 0; i < index - 1; i++) {
-                n = n.next;
-            }
-            node.next = n.next;
-            n.next = node;
-        }
-
-    }
-
-    public void deleteAt(int index) {
-        if (index == 0) {
-            head = head.next;
-        } else {
-            Node n = head;
-            Node n1 = null;
-            for (int i = 0; i < index-1; i++) {
-                n = n.next;
-            }
-           n1 = n.next;
-           n.next = n1.next;
-        }
     }
 
     public void Show() {
-        Node node = head;
-        while (node.next != null) {
-            System.out.println(node.data);
-            node = node.next;
+        Node n = head;
+        while (n.next != null) {
+            System.out.println(n.data);
+            n = n.next;
         }
-        System.out.println(node.data);
-
+        System.out.println(n.data);
     }
+
 }
